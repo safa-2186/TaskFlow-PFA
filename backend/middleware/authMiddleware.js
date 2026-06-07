01e778080930da3c125557a57dfe1a3ca28fadb4
safa <safa.bouguilime@gmail.com>
2026-06-06 13:25:40 -1200

    feat(auth): improve auth middleware logic

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split('')[1];
+
+    // if there's no token, block the request immediately (401 = Unauthorized)
+    if (!token) {
+        return res.status(401).json({message: 'Access denied. No token provided.'});
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+
+    } catch (err) {
+        return res.status(401).json({message: 'Invalid or expired token.'});
+    }
+
+};
